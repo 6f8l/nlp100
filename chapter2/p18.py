d181f39377442c683210bdbbb1d574013bde5529
6f8l <atsuki02jojo@gmail.com>
2020-06-24 06:16:53 +0900

    Fix lint

--- chapter2/p18.py
+++ chapter2/p18.py
@@ -3,8 +3,8 @@
 # df = pd.read_csv('./popular-names.txt', sep='\t', header=None)
 # print(df.sort_values(2, ascending=False))
 
-fname = 'popular-names.txt'
-lines = open(fname).readlines()
+FNAME = 'popular-names.txt'
+lines = open(FNAME).readlines()
 lines.sort(key=lambda line: float(line.split('\t')[2]), reverse=True)
 
 for line in lines:
--- chapter2/p19.py
+++ chapter2/p19.py
@@ -1,10 +1,10 @@
 import collections
 
-fname = 'popular-names.txt'
-lines = open(fname).readlines()
+FNAME = 'popular-names.txt'
+lines = open(FNAME).readlines()
 
 names = [line.split()[0] for line in lines]
 name_c = collections.Counter(names)
 
-name_sorted = sorted(name_c.items(), key=lambda x:x[1], reverse=True)
+name_sorted = sorted(name_c.items(), key=lambda x: x[1], reverse=True)
 print(name_sorted)
