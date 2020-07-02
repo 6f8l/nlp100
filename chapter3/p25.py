0aea1fa15af2651e4bd29ad7bf470300b4965abe
6f8l <atsuki02jojo@gmail.com>
2020-07-02 21:38:34 +0900

    Impl 25

---
+++ chapter3/p25.py
@@ -0,0 +1,41 @@
+import gzip
+import json
+import re
+
+FNAME = 'jawiki-country.json.gz'
+def extract_uk():
+    with gzip.open(FNAME, 'rt') as data_file:
+        for row in data_file:
+            data_json = json.loads(row)
+            if data_json['title'] == 'イギリス':
+                return data_json['text']
+    raise ValueError('Not found England articles.')
+
+if __name__ == "__main__":
+    rstart = re.compile(r"^{{基礎情報(.+)")
+    rend = re.compile(r"^}}")
+    r = re.compile(r"^\|(.+) = (.+)")
+    doc = extract_uk().split("\n")
+
+    ENTER = False
+    END = False
+
+    d = {}
+
+    for line in doc:
+        if END:
+            break
+        if not ENTER:
+            if rstart.match(line):
+                ENTER = True
+                continue
+        elif ENTER:
+            for it in r.finditer(line):
+                d[it.group(1)] = it.group(2)
+            if rend.match(line):
+                END = True
+                continue
+        else:
+            pass
+    for k, v in d.items():
+        print("{}: {}".format(k, v))
